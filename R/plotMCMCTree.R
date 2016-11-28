bdf599f4ae376952ccbc5112cbd0f551a2768760
MN Puttick <mp1728@eduroam-134-135.nomadic.bris.ac.uk>
2016-11-28 08:13:02 +0000

    first commit

--- R/plotMCMCTree.R
+++ R/plotMCMCTree.R
@@ -25,53 +25,82 @@
 #' bound <- estimateBound(minAge=minimumTimes[1], maxAge=maximumTimes[1], monoGroups=monophyleticGroups[[1]], minProb=0.1, rightTail=0.1, phy=apeTree, plot=F)
 #' plotMCMCTree(parameters=bound$parameters, method="bound", title="uniformPlot", upperTime=maximumTimes[1]+1)
 
-plotMCMCTree <- function(parameters, method=c("skewT", "skewNormal", "cauchy", "gamma", "bound"), title, upperTime, lowerTime=0, plotMCMCTreeData=FALSE) {
-		
-	if(method == "skewT") {
-		timeScale <- c(lowerTime, upperTime)
-		curve(dst(x, xi=parameters[1], omega=parameters[2], alpha=parameters[3], nu=parameters[4]),lty=1, col="grey20", lwd=2, xlim=timeScale, xaxs="i", yaxs="i", bty="l", las=1, ylab="density", xlab="Ma", main=paste(title), cex=0.8)
-		if(plotMCMCTreeData == TRUE) {
-			time <- seq(0, timeScale[2], 0.01)
-			density <- dst(time, xi=parameters[1], omega=parameters[2], alpha=parameters[3], nu=parameters[4])
-			return(cbind(time, density))			
-			}
-		}
-	
-	if(method == "skewNormal") {	
-		timeScale <- c(lowerTime, upperTime)
-		curve(dsn(x, xi=parameters[1], omega=parameters[2], alpha=parameters[3]),lty=1, col="grey20", xlim=timeScale, lwd=1, xpd=T, xaxs="i", yaxs="i", bty="l", las=1, ylab="density", xlab="Ma", main=paste(title), cex=0.8)		
-		if(plotMCMCTreeData == TRUE) {
-			time <- seq(0, timeScale[2], 0.01)
-			density <- dsn(time, xi=parameters[1], omega=parameters[2], alpha=parameters[3])
-			return(cbind(time, density))			
-			}
-		}
-	
-	if(method == "cauchy") {	
-		timeScale <- c(lowerTime, upperTime)
-		plot(cauchyMCMCTree(xRange=timeScale, tL=parameters[1], p=parameters[2], c=parameters[3], minProb=parameters[4]), type="l", lty=1, col="grey20", xlim=timeScale, lwd=1, xpd=T, xaxs="i", yaxs="i", bty="l", las=1, ylab="density", xlab="Ma", main=paste(title), cex=0.8)
-		}	
-		if(plotMCMCTreeData == TRUE) {
-			out <- cauchyMCMCTree(xRange=timeScale, tL=parameters[1], p=parameters[2], c=parameters[3], minProb=parameters[4])
-			return(out)
-			}
-	
-	if(method == "gamma") {	
-		timeScale <- c(lowerTime, upperTime)
-		curve(dgamma(x, shape=parameters[1],rate=parameters[2]), lty=1, col="grey20", xlim=timeScale, lwd=1, xpd=T, xaxs="i", yaxs="i", bty="l", las=1, ylab="density", xlab="Ma", main=paste(title), cex=0.8)
-		if(plotMCMCTreeData == TRUE) {
-			time <- seq(0, timeScale[2], 0.01)
-			density <- dgamma(time, shape=parameters[1], rate=parameters[2])
-			return(cbind(time, density))			
-			}
-		}		
-		
-	if(method == "bound") {	
-		timeScale <- c(lowerTime, upperTime)
-		plot(uniformMCMCTree(xRange=timeScale, tL=parameters[1], tU=parameters[2], minProb=parameters[3], maxProb=parameters[4]), lty=1, col="grey20", type="l", xlim=timeScale, lwd=1, xpd=T, xaxs="i", yaxs="i", bty="l", las=1, ylab="density", xlab="Ma", main=paste(title), cex=0.8)
-		if(plotMCMCTreeData == TRUE) {
-			out <- uniformMCMCTree(xRange=timeScale, tL=parameters[1], tU=parameters[2], minProb=parameters[3], maxProb=parameters[4])
-			return(out)
-			}
-		}	
+plotMCMCTree <- function (parameters, method = c("skewT", "skewNormal", "cauchy", 
+    "gamma", "bound"), title, upperTime, lowerTime = 0, plotMCMCTreeData = FALSE) 
+{
+    if (method == "skewT") {
+        timeScale <- c(lowerTime, upperTime)
+        if (plotMCMCTreeData == TRUE) {
+        curve(dst(x, xi = parameters[1], omega = parameters[2], 
+            alpha = parameters[3], nu = parameters[4]), lty = 1, 
+            col = "grey20", lwd = 2, xlim = timeScale, xaxs = "i", 
+            yaxs = "i", bty = "l", las = 1, ylab = "density", 
+            xlab = "Ma", main = paste(title), cex = 0.8)
+  			} else {
+            time <- seq(0, timeScale[2], 0.01)
+            density <- dst(time, xi = parameters[1], omega = parameters[2], 
+                alpha = parameters[3], nu = parameters[4])
+            return(cbind(time, density))
+        }
+    }
+    if (method == "skewNormal") {
+        timeScale <- c(lowerTime, upperTime)
+        if (plotMCMCTreeData == TRUE) {
+        curve(dsn(x, xi = parameters[1], omega = parameters[2], 
+            alpha = parameters[3]), lty = 1, col = "grey20", 
+            xlim = timeScale, lwd = 1, xpd = T, xaxs = "i", yaxs = "i", 
+            bty = "l", las = 1, ylab = "density", xlab = "Ma", 
+            main = paste(title), cex = 0.8)
+  			} else {
+            time <- seq(0, timeScale[2], 0.01)
+            density <- dsn(time, xi = parameters[1], omega = parameters[2], 
+                alpha = parameters[3])
+            return(cbind(time, density))
+        }
+    }
+    if (method == "cauchy") {
+        timeScale <- c(lowerTime, upperTime)
+        if (plotMCMCTreeData == TRUE) {
+        plot(cauchyMCMCTree(xRange = timeScale, tL = parameters[1], 
+            p = parameters[2], c = parameters[3], minProb = parameters[4]), 
+            type = "l", lty = 1, col = "grey20", xlim = timeScale, 
+            lwd = 1, xpd = T, xaxs = "i", yaxs = "i", bty = "l", 
+            las = 1, ylab = "density", xlab = "Ma", main = paste(title), 
+            cex = 0.8)
+  			} else {
+        out <- cauchyMCMCTree(xRange = timeScale, tL = parameters[1], 
+            p = parameters[2], c = parameters[3], minProb = parameters[4])
+        return(out)
+        }
+    }
+    if (method == "gamma") {
+        timeScale <- c(lowerTime, upperTime)
+        if (plotMCMCTreeData == TRUE) {
+        curve(dgamma(x, shape = parameters[1], rate = parameters[2]), 
+            lty = 1, col = "grey20", xlim = timeScale, lwd = 1, 
+            xpd = T, xaxs = "i", yaxs = "i", bty = "l", las = 1, 
+            ylab = "density", xlab = "Ma", main = paste(title), 
+            cex = 0.8)
+        	} else {
+            time <- seq(0, timeScale[2], 0.01)
+            density <- dgamma(time, shape = parameters[1], rate = parameters[2])
+            return(cbind(time, density))
+        }
+    }
+    if (method == "bound") {
+        timeScale <- c(lowerTime, upperTime)
+        if (plotMCMCTreeData == F) {
+        plot(uniformMCMCTree(xRange = timeScale, tL = parameters[1], 
+            tU = parameters[2], minProb = parameters[3], maxProb = parameters[4]), 
+            lty = 1, col = "grey20", type = "l", xlim = timeScale, 
+            lwd = 1, xpd = T, xaxs = "i", yaxs = "i", bty = "l", 
+            las = 1, ylab = "density", xlab = "Ma", main = paste(title), 
+            cex = 0.8)
+            } else {
+            out <- uniformMCMCTree(xRange = timeScale, tL = parameters[1], 
+                tU = parameters[2], minProb = parameters[3], 
+                maxProb = parameters[4])
+            return(out)
+        }
+    }
 }
