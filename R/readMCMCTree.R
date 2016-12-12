32f6d9dadac279ddba960081d59b1acd65c8da04
MN Puttick <mp1728@eduroam-70-157.nomadic.bris.ac.uk>
2016-12-12 10:33:36 +0000

    first commit

--- R/readMCMCTree.R
+++ R/readMCMCTree.R
@@ -10,25 +10,33 @@
 #' readMCMCTree()
 
 readMCMCTree <- function(inputPhy) {
-	tree <- scan(paste0(inputPhy), what="", sep="\t")
-	phys <- gsub('\\[.*?\\]', '', tree)
-	phy <- read.tree(text=phys[4])
-	phy <- ladderize(phy)
-	phyInt <- phy
-	phyInt$node.label <- 1:Nnode(phy)
-	phyInt$edge.length <- NULL
-	phyInt$tip.label[which(unlist(gregexpr("[0-9]", phyInt$tip.label)) != -1)] <- paste0(sample(letters, replace=T, 3), collapse="")
-	nodingOrder <- as.numeric(as.character(unlist(strsplit(write.tree(phyInt)[[1]], "[^0-9]+"))[-1]))
-	openB <- gregexpr("[[]", tree[4])[[1]]; closeB <- gregexpr("[]]", tree[4])[[1]]
-	stepOne <- sapply(1:length(openB), function(r) substr(tree[4], openB[r], closeB[r]))
-	endPoint <- gregexpr("[}]", stepOne[1])[[1]]-1; startPoint <- gregexpr("[{]", stepOne[1])[[1]]+1
-	stepOne <- stepOne[match(104:(Nnode(phy)+Ntip(phy)), nodingOrder+Ntip(phy))]
-	CIs <- t(sapply(length(stepOne):1, function(k) as.numeric(strsplit(substr(stepOne[k], startPoint, endPoint), ",")[[1]])))
-	mean <- branching.times(phy)
-	allAges <- cbind(mean, CIs)
-	output <- list()
-	output$apePhy <- phy
-	colnames(allAges) <- c("mean", "95%_lower", "95%_upper")
-	output$nodeAges <- allAges
-	return(ouput)
+ tree <- scan(paste0(inputPhy), what = "", sep = "\t")
+    phys <- gsub("\\[.*?\\]", "", tree)
+    phy <- read.tree(text = phys[4])
+    phy <- ladderize(phy)
+    phyInt <- phy
+    phyInt$node.label <- 1:Nnode(phy)
+    phyInt$edge.length <- NULL
+    phyInt$tip.label[which(unlist(gregexpr("[0-9]", phyInt$tip.label)) != 
+        -1)] <- paste0(sample(letters, replace = T, 3), collapse = "")
+    nodingOrder <- as.numeric(as.character(unlist(strsplit(write.tree(phyInt)[[1]], 
+        "[^0-9]+"))[-1]))
+    openB <- gregexpr("[[]", tree[4])[[1]]
+    closeB <- gregexpr("[]]", tree[4])[[1]]
+    stepOne <- sapply(1:length(openB), function(r) substr(tree[4], 
+        openB[r], closeB[r]))
+    endPoint <- gregexpr("[}]", stepOne[1])[[1]] - 1
+    startPoint <- gregexpr("[{]", stepOne[1])[[1]] + 1
+    stepOne <- stepOne[match(104:(Nnode(phy) + Ntip(phy)), nodingOrder + 
+        Ntip(phy))]
+    CIs <- sapply(length(stepOne):1, function(k) as.numeric(strsplit(substr(stepOne[k], 
+    startPoint, endPoint), ",")[[1]]))    
+    CIs <- apply(t(CIs), 2, rev)
+    mean <- branching.times(phy)
+    allAges <- cbind(mean, CIs)
+    output <- list()
+    output$apePhy <- phy
+    colnames(allAges) <- c("mean", "95%_lower", "95%_upper")
+    output$nodeAges <- allAges
+    return(output)
 }
